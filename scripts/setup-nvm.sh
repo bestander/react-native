8028f2f35ce1ac5424f2a03cd354d774ea3a4ea3
Konstantin Raev <bestander@fb.com>
2016-03-11 17:18:25 +0000

    playing with sdk

---
+++ scripts/setup-nvm.sh
@@ -0,0 +1,11 @@
+mkdir ~/.nvm
+ls -al ~/.nvm
+brew install nvm
+export NVM_DIR=~/.nvm
+ls -al ~/.nvm
+echo $NVM_DIR
+ls -al $NVM_DIR
+ls $(brew --prefix nvm)
+source $(brew --prefix nvm)/nvm.sh
+ls -al $NVM_DIR
+ls ~/.nvm
